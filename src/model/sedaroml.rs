37c31f47ec3f9d41c57070d530f93f9f2d08636b
Bas Welsh <sebastian.welsh@sedarotech.com>
2024-09-04 09:26:56 -0400

    Initial model diff capability

--- src/model/sedaroml.rs
+++ src/model/sedaroml.rs
@@ -120,6 +120,76 @@ impl Model {
   pub fn to_pretty_string(&self) -> String {
     serde_json::to_string_pretty(&self).unwrap()
   }
+
+  pub fn diff(&self, new: &Model) -> ModelDiff {
+    let mut added_blocks = IndexMap::new();
+    let mut removed_blocks = IndexMap::new();
+    let mut updated_blocks = IndexMap::new();
+    for (block_id, new_block) in new.blocks.iter() {
+      match self.blocks.get(block_id) {
+        Some(old_block) => {
+          let mut added_fields = IndexMap::new();
+          let mut removed_fields = IndexMap::new();
+          let mut updated_fields = IndexMap::new();
+          for (field_key, field_value) in new_block.iter() {
+            match old_block.get(field_key) {
+              Some(old_value) => {
+                if old_value != field_value {
+                  updated_fields.insert(field_key.clone(), ValueDiff { old_value: old_value.clone(), new_value: field_value.clone() });
+                }
+              },
+              None => {
+                added_fields.insert(field_key.clone(), field_value.clone());
+              },
+            }
+          }
+          for (field_key, field_value) in old_block.iter() {
+            match new_block.get(field_key) {
+              Some(_) => {},
+              None => {
+                removed_fields.insert(field_key.clone(), field_value.clone());
+              },
+            }
+          }
+          if !added_fields.is_empty() || !removed_fields.is_empty() || !updated_fields.is_empty() {
+            updated_blocks.insert(block_id.clone(), BlockDiff { added_fields, removed_fields, updated_fields });
+          }
+        },
+        None => {
+          added_blocks.insert(block_id.clone(), new_block.clone());
+        },
+      }
+    }
+    for (block_id, old_block) in self.blocks.iter() {
+      if !new.blocks.contains_key(block_id) {
+        removed_blocks.insert(block_id.clone(), old_block.clone());
+      }
+    }
+    let mut added_fields = IndexMap::new();
+    let mut removed_fields = IndexMap::new();
+    let mut updated_fields = IndexMap::new();
+    for (field_key, field_value) in new.root.iter() {
+      match self.root.get(field_key) {
+        Some(old_value) => {
+          if old_value != field_value {
+            updated_fields.insert(field_key.clone(), ValueDiff { old_value: old_value.clone(), new_value: field_value.clone() });
+          }
+        },
+        None => {
+          added_fields.insert(field_key.clone(), field_value.clone());
+        },
+      }
+    }
+    for (field_key, field_value) in self.root.iter() {
+      match new.root.get(field_key) {
+        Some(_) => {},
+        None => {
+          removed_fields.insert(field_key.clone(), field_value.clone());
+        },
+      }
+    }
+    ModelDiff { added_blocks, removed_blocks, updated_blocks, root: BlockDiff { added_fields, removed_fields, updated_fields } }
+  }
 }
 
 pub fn read_model(file_path: &str) -> Result<Model, ModelError> {
@@ -133,3 +203,225 @@ pub fn read_model(file_path: &str) -> Result<Model, ModelError> {
 pub fn write_model(file_path: &str, model: &Model) -> Result<(), ModelError> {
   write_json(file_path, &model.to_pretty_string())
 }
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct ValueDiff {
+  old_value: Value,
+  new_value: Value,
+}
+
+impl PartialEq for ValueDiff {
+  fn eq(&self, other: &Self) -> bool {
+      self.old_value == other.old_value && self.new_value == other.new_value
+  }
+}
+impl Eq for ValueDiff {}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct BlockDiff {
+  added_fields: IndexMap<String, Value>,
+  removed_fields: IndexMap<String, Value>,
+  updated_fields: IndexMap<String, ValueDiff>,
+}
+
+impl PartialEq for BlockDiff {
+  fn eq(&self, other: &Self) -> bool {
+      self.added_fields == other.added_fields &&
+      self.removed_fields == other.removed_fields &&
+      self.updated_fields == other.updated_fields
+  }
+}
+impl Eq for BlockDiff {}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct ModelDiff {
+  added_blocks: IndexMap<String, Block>,
+  removed_blocks: IndexMap<String, Block>,
+  updated_blocks: IndexMap<String, BlockDiff>,
+  root: BlockDiff,
+}
+
+impl PartialEq for ModelDiff {
+  fn eq(&self, other: &Self) -> bool {
+      self.added_blocks == other.added_blocks &&
+      self.removed_blocks == other.removed_blocks &&
+      self.updated_blocks == other.updated_blocks &&
+      self.root == other.root
+  }
+}
+impl Eq for ModelDiff {}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use serde_json::json;
+
+  #[test]
+  fn test_model_diff() {
+
+    assert_eq!(
+      ValueDiff { old_value: Value::String("old".to_string()), new_value: Value::String("new".to_string()) },
+      ValueDiff { old_value: Value::String("old".to_string()), new_value: Value::String("new".to_string()) },
+    );
+
+    assert_eq!(
+      {
+        let mut map = IndexMap::new();
+        map.insert("key".to_string(), Value::String("value".to_string()));
+        map
+      },
+      {
+        let mut map = IndexMap::new();
+        map.insert("key".to_string(), Value::String("value".to_string()));
+        map
+      }
+  );
+
+    let mut old = Model::new();
+    let new =  Model::new();
+
+    assert_eq!(old.diff(&new), ModelDiff {
+      added_blocks: IndexMap::new(),
+      removed_blocks: IndexMap::new(),
+      updated_blocks: IndexMap::new(),
+      root: BlockDiff {
+        added_fields: IndexMap::new(),
+        removed_fields: IndexMap::new(),
+        updated_fields: IndexMap::new(),
+      },
+    });
+    assert_eq!(new.diff(&old), ModelDiff {
+      added_blocks: IndexMap::new(),
+      removed_blocks: IndexMap::new(),
+      updated_blocks: IndexMap::new(),
+      root: BlockDiff {
+        added_fields: IndexMap::new(),
+        removed_fields: IndexMap::new(),
+        updated_fields: IndexMap::new(),
+      },
+    });
+
+    old.root.insert("name".to_string(), json!("root"));
+    old.blocks.insert("1".to_string(), {
+      let mut block = Block::new();
+      block.insert("name".to_string(), json!("block1"));
+      block
+    });
+
+    assert_eq!(old.diff(&new), ModelDiff {
+      added_blocks: IndexMap::new(),
+      removed_blocks: {
+        let mut removed_blocks = IndexMap::new();
+        removed_blocks.insert("1".to_string(), {
+          let mut block = Block::new();
+          block.insert("name".to_string(), json!("block1"));
+          block
+        });
+        removed_blocks
+      },
+      updated_blocks: IndexMap::new(),
+      root: BlockDiff {
+        added_fields: IndexMap::new(),
+        removed_fields: {
+          let mut removed_fields = IndexMap::new();
+          removed_fields.insert("name".to_string(), Value::String("root".to_string()));
+          removed_fields
+        },
+        updated_fields: IndexMap::new(),
+      },
+    });
+
+    assert_eq!(new.diff(&old), ModelDiff {
+      added_blocks: {
+        let mut added_blocks = IndexMap::new();
+        added_blocks.insert("1".to_string(), {
+          let mut block = Block::new();
+          block.insert("name".to_string(), json!("block1"));
+          block
+        });
+        added_blocks
+      },
+      removed_blocks: IndexMap::new(),
+      updated_blocks: IndexMap::new(),
+      root: BlockDiff {
+        added_fields: {
+          let mut added_fields = IndexMap::new();
+          added_fields.insert("name".to_string(), Value::String("root".to_string()));
+          added_fields
+        },
+        removed_fields: IndexMap::new(),
+        updated_fields: IndexMap::new(),
+      },
+    });
+
+    let mut new = old.clone();
+    new.root.insert("name".to_string(), json!("root2"));
+    new.blocks.get_mut("1").unwrap().insert("name".to_string(), json!("block2"));
+
+    assert_eq!(old.diff(&new), ModelDiff {
+      added_blocks: IndexMap::new(),
+      removed_blocks: IndexMap::new(),
+      updated_blocks: {
+        let mut updated_blocks = IndexMap::new();
+        updated_blocks.insert("1".to_string(), BlockDiff {
+          added_fields: IndexMap::new(),
+          removed_fields: IndexMap::new(),
+          updated_fields: {
+            let mut updated_fields = IndexMap::new();
+            updated_fields.insert("name".to_string(), ValueDiff {
+              old_value: Value::String("block1".to_string()),
+              new_value: Value::String("block2".to_string()),
+            });
+            updated_fields
+          },
+        });
+        updated_blocks
+      },
+      root: BlockDiff {
+        added_fields: IndexMap::new(),
+        removed_fields: IndexMap::new(),
+        updated_fields: {
+          let mut updated_fields = IndexMap::new();
+          updated_fields.insert("name".to_string(), ValueDiff {
+            old_value: Value::String("root".to_string()),
+            new_value: Value::String("root2".to_string()),
+          });
+          updated_fields
+        },
+      },
+    });
+    assert_eq!(new.diff(&old), ModelDiff {
+      added_blocks: IndexMap::new(),
+      removed_blocks: IndexMap::new(),
+      updated_blocks: {
+        let mut updated_blocks = IndexMap::new();
+        updated_blocks.insert("1".to_string(), BlockDiff {
+          added_fields: IndexMap::new(),
+          removed_fields: IndexMap::new(),
+          updated_fields: {
+            let mut updated_fields = IndexMap::new();
+            updated_fields.insert("name".to_string(), ValueDiff {
+              old_value: Value::String("block2".to_string()),
+              new_value: Value::String("block1".to_string()),
+            });
+            updated_fields
+          },
+        });
+        updated_blocks
+      },
+      root: BlockDiff {
+        added_fields: IndexMap::new(),
+        removed_fields: IndexMap::new(),
+        updated_fields: {
+          let mut updated_fields = IndexMap::new();
+          updated_fields.insert("name".to_string(), ValueDiff {
+            old_value: Value::String("root2".to_string()),
+            new_value: Value::String("root".to_string()),
+          });
+          updated_fields
+        },
+      },
+    });
+  }
+}
+
